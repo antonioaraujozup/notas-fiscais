c7a153a2c2108de4bb7999e340dab340d4c6a21e
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-07-26 09:22:17 -0300

    Commit inicial

---
+++ src/test/java/br/com/zup/edu/commerce/notasfiscais/NotasFiscaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.commerce.notasfiscais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasFiscaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
